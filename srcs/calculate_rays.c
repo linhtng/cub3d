c5e0d04186d191e3de2b11a358fe9594259ba7b2
Jenny Boucher <jebouche@c3r5p3.hive.fi>
2023-07-26 18:20:08 +0300

    add functioning horizontal and vertical shooting before cleanup

--- srcs/calculate_rays.c
+++ srcs/calculate_rays.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:49:54 by jebouche          #+#    #+#             */
-/*   Updated: 2023/07/25 17:51:26 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/07/26 17:48:56 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,245 +31,98 @@ float	deg_to_rad(float degrees)
 	// printf("DEG: %f, RAD: %f", degrees, radians);//printed with correct values
 	return (radians);
 }
-
-
 void	shoot_one_ray_horizontal(t_cubed *cubed, t_scene *scene, float angle)
 {
-	scene = (void *) scene;
-	printf("HORIZONTAL \n");
 	t_ray_calc ray;
 
-	//INITAL shot
-	//get y
-	if (angle >=0 && angle < 180)//looking 'up'
-		ray.map_intersection.y = cubed->player.location.y / CELL_SIZE * CELL_SIZE - 1;
-	else
-		ray.map_intersection.y = cubed->player.location.y / CELL_SIZE * CELL_SIZE + CELL_SIZE;
-	//get x
-	if (angle >=0 && angle < 180) // looking 'up'
-		ray.map_intersection.x = cubed->player.location.x + (ft_abs(ray.map_intersection.y - cubed->player.location.y) / tan(deg_to_rad(angle)));//looking 'down' is wrong
-	else
-		ray.map_intersection.x = cubed->player.location.x - (ft_abs(cubed->player.location.y - ray.map_intersection.y) / tan(deg_to_rad(angle)));
-	// TEST_PRINT
-	printf("PLAYER LOCATION X,Y: %d,%d\n", cubed->player.location.x, cubed->player.location.y);//
-	printf("FIRST INTERSECTION:\n");//
-	printf("MAP X,Y: %d, %d\n", ray.map_intersection.x, ray.map_intersection.y);//
-	if (angle >=0 && angle < 180)
-		ray.y_inc = CELL_SIZE * -1; //moved out of while loop
-	else 
-		ray.y_inc = CELL_SIZE;
-	if (angle >= 90 && angle < 270)
-		ray.x_inc = ft_abs(CELL_SIZE / tan(deg_to_rad(angle))) * -1;//check for 0 vals
-	else
-		ray.x_inc = ft_abs(CELL_SIZE / tan(deg_to_rad(angle)));
-	while (ray.map_intersection.y < CELL_SIZE * scene->lines && ray.map_intersection.y > 0)
-	{	
-		ray.map_intersection.y += ray.y_inc;
-		ray.map_intersection.x += ray.x_inc;
-		printf("NEXT INTERSECTION:\n");
-		printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
+	ray.angle = angle;
+	ray.dof = 0;
+	ray.cotan = 1.0 / tan(deg_to_rad(ray.angle));//or cotan???
+	if (sin(deg_to_rad(ray.angle)) > 0.001) //looking down ray.angle > 180
+	{
+		ray.map.y = ((int)cubed->player.location.y / CELL_SIZE) * CELL_SIZE - 0.00001;
+		ray.map.x = cubed->player.location.x + ((cubed->player.location.y - ray.map.y) * ray.cotan);
+		ray.hd.y = -CELL_SIZE;
+		ray.hd.x = -ray.hd.y * ray.cotan;
+	}
+	else if (sin(deg_to_rad(ray.angle)) < -0.001) //looking up ray.angle < 180
+	{
+		ray.map.y = ((int)cubed->player.location.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+		ray.map.x = cubed->player.location.x + ((cubed->player.location.y - ray.map.y) * ray.cotan);
+		ray.hd.y = CELL_SIZE;
+		ray.hd.x = -ray.hd.y * ray.cotan;		
+	}
+	else //will not hit a horizontal if (ray.angle == 0 || ray.angle == 180)
+	{
+		ray.map.x = cubed->player.location.x;
+		ray.map.y = cubed->player.location.y;
+		ray.dof = 8;
+	}
+	//check hit wall
+	while (ray.dof < MAX_DOF)
+	{
+		ray.grid.x = (int)ray.map.x / CELL_SIZE;
+		ray.grid.y = (int)ray.map.y / CELL_SIZE;
+		printf("GRID XY: %f, %f DOF: %i\n", ray.grid.x, ray.grid.y, ray.dof);
+		if (ray.grid.y >= 0 && (ray.grid.x < scene->columns && ray.grid.y < scene->lines && scene->map[(int)ray.grid.y][(int)ray.grid.x] == '1')) //(ray.grid.x < 0 || ray.grid.y < 0 || ray.grid.x > scene->columns || ray.grid.y < scene->lines)
+			ray.dof = 8;
+		else
+		{
+			ray.map.x += ray.hd.x;
+			ray.map.y += ray.hd.y;
+			(ray.dof)++;
+		}
 	}
 	cubed->player.location.color = 0xFFFFFF;//
-	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);//
-
+	ft_bresenham(cubed->player.location, ray.map, cubed->player_img);//
 }
-	// ray.dir_modifier.x = 1;
-	// ray.dir_modifier.y = 1;
-//TODO shoot rays to vericals
+
+
+
 void	shoot_one_ray_vertical(t_cubed *cubed, t_scene *scene, float angle)
 {
 	scene = (void *) scene;
-	printf("VERTICAL \n");
 	t_ray_calc ray;
 
-	//INITAL shot
-	//get x
-	if (angle >= 90 && angle < 270) // looking 'left'
-		ray.map_intersection.x = (cubed->player.location.x / CELL_SIZE) * CELL_SIZE - 1;
-	else
-		ray.map_intersection.x = (cubed->player.location.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
-	//get y
-	// if (angle >=0 && angle < 180)//looking 'up'
-		ray.map_intersection.y = cubed->player.location.y + ((cubed->player.location.x - ray.map_intersection.x) * tan(deg_to_rad(angle)));
-	// else
-		// ray.map_intersection.y = cubed->player.location.y + ((cubed->player.location.x - ray.map_intersection.x) * tan(deg_to_rad(angle)));
-	// TEST_PRINT
-	printf("PLAYER LOCATION X,Y: %d,%d\n", cubed->player.location.x, cubed->player.location.y);//
-	printf("FIRST INTERSECTION:\n");//
-	printf("MAP X,Y: %d, %d\n", ray.map_intersection.x, ray.map_intersection.y);//
-// 	printf("MODIFIERS X: %i, Y: %i\n", ray.dir_modifier.x, ray.dir_modifier.y);//
-
-
-	if (angle >= 90 && angle < 270)
-		ray.x_inc = CELL_SIZE * -1; //moved out of while loop
-	else 
-		ray.x_inc = CELL_SIZE;
-	if (angle >=0 && angle < 180)
-		ray.y_inc = ft_abs(CELL_SIZE * tan(deg_to_rad(angle))) * -1;//check for 0 vals
-	else
-		ray.y_inc = ft_abs(CELL_SIZE * tan(deg_to_rad(angle)));
-
-	// ray.x_inc = CELL_SIZE * ray.dir_modifier.x;
-	// ray.y_inc = (ft_abs(CELL_SIZE * tan(deg_to_rad(angle)))) * ray.dir_modifier.y;//check for 0 vals///////
-	while (ray.map_intersection.y < CELL_SIZE * scene->lines && ray.map_intersection.y > 0) //changed from x
-	{	
-		ray.map_intersection.y += ray.y_inc;
-		ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;
-		ray.map_intersection.x += ray.x_inc;
-		ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);
-		//PRINT to CHECK
-		printf("NEXT INTERSECTION:\n");
-		printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
+	ray.angle = angle;
+	ray.dof = 0;
+	ray.tan = tan(deg_to_rad(ray.angle));
+	if (cos(deg_to_rad(ray.angle)) < -0.001) //looking left?
+	{
+		ray.map.x = ((int)cubed->player.location.x / CELL_SIZE) * CELL_SIZE - 0.00001;
+		ray.map.y = cubed->player.location.y + ((cubed->player.location.x - ray.map.x) * ray.tan);
+		ray.vd.x = -CELL_SIZE;
+		ray.vd.y = -ray.vd.x * ray.tan;
 	}
-	cubed->player.location.color = 0xFF00FF;//
-	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);//
+	else if (cos(deg_to_rad(ray.angle)) > 0.001) //looking right?
+	{
+		ray.map.x = ((int)cubed->player.location.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+		ray.map.y = cubed->player.location.y + ((cubed->player.location.x - ray.map.x) * ray.tan);
+		ray.vd.x = CELL_SIZE;
+		ray.vd.y = -ray.vd.x * ray.tan;		
+	}
+	else //will not hit a horizontal if (ray.angle == 0 || ray.angle == 180)
+	{
+		ray.map.x = cubed->player.location.x;
+		ray.map.y = cubed->player.location.y;
+		ray.dof = MAX_DOF;
+	}
+	//check hit wall
+	while (ray.dof < MAX_DOF)
+	{
+		ray.grid.x = (int)ray.map.x / CELL_SIZE;
+		ray.grid.y = (int)ray.map.y / CELL_SIZE;
+		printf("GRID XY: %f, %f DOF: %i\n", ray.grid.x, ray.grid.y, ray.dof);
+	
+		if (ray.grid.y >= 0  && ray.grid.x  >= 0 && ray.grid.x < scene->columns && ray.grid.y < scene->lines && scene->map[(int)ray.grid.y][(int)ray.grid.x] == '1')
+			ray.dof = MAX_DOF;
+		else
+		{
+			ray.map.x += ray.vd.x;
+			ray.map.y += ray.vd.y;
+			(ray.dof)++;
+		}
+	}
+	cubed->player.location.color = 0x000FFF;//
+	ft_bresenham(cubed->player.location, ray.map, cubed->player_img);//
 }
-
-// TODO: shoot one ray
-// currently based on CELL SIZE for minimap
-// void	shoot_one_ray_horizontal(t_cubed *cubed, t_scene *scene, float angle)
-// {
-// 	printf("HORIZONTAL \n");
-// 	t_ray_calc ray;
-
-// 	// scene = (void *) scene;
-// 	ray.dir_modifier.x = 1;
-// 	if (angle > 90 && angle < 270)
-// 		ray.dir_modifier.x = -1;
-// 	ray.dir_modifier.y = 1;
-// 	if (angle < 180)//changed to be - for 'up'
-// 		ray.dir_modifier.y = -1;
-// 	if (ray.dir_modifier.y == 1) //find intersection
-// 		ray.map_intersection.y = ((cubed->player.location.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE);
-// 	else
-// 		ray.map_intersection.y = (cubed->player.location.y / CELL_SIZE) * CELL_SIZE - 1; //removed cell size
-// 	ray.y_inc = (cubed->player.location.y - ray.map_intersection.y);
-// 	ray.map_intersection.x = cubed->player.location.x + (ray.y_inc / tan(deg_to_rad(angle)));//check about 0 values here
-// 	ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;//TODO remove grid update
-// 	ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);//TODO remove grid update
-// 	printf("PLAYER LOCATION X,Y: %d,%d\n", cubed->player.location.x, cubed->player.location.y);//
-// 	printf("FIRST INTERSECTION:\n");//
-// 	printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);//
-// 	printf("MODIFIERS X: %i, Y: %i\n", ray.dir_modifier.x, ray.dir_modifier.y);//
-	
-	// ray.y_inc = CELL_SIZE * ray.dir_modifier.y; //moved out of while loop
-	// ray.x_inc = (ft_abs(CELL_SIZE / tan(deg_to_rad(angle)))) * ray.dir_modifier.x;//check for 0 vals
-	// while (ray.map_intersection.y < CELL_SIZE * scene->lines && ray.map_intersection.y > 0)
-	// {	
-	// 	ray.map_intersection.y += ray.y_inc;
-	// 	ray.map_intersection.x += ray.x_inc;
-	// 	printf("NEXT INTERSECTION:\n");
-	// 	printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-	// }
-// 	ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;//TODO remove grid update
-// 	ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);//TODO remove grid update
-// 	cubed->player.location.color = 0xFFFF00;//
-// 	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);//
-// }
-// //TODO shoot rays to vericals
-// void	shoot_one_ray_vertical(t_cubed *cubed, t_scene *scene, float angle)
-// {
-// 	printf("VERTICAL \n");
-// 	t_ray_calc ray;
-
-// 	scene = (void *) scene;
-// 	//determine direction
-// 	ray.dir_modifier.x = 1;
-// 	if (angle > 90 && angle < 270)
-// 		ray.dir_modifier.x = -1;
-// 	ray.dir_modifier.y = 1;
-// 	if (angle < 180)//changed to be - for 'up'
-// 		ray.dir_modifier.y = -1;
-// 	//find first intersection
-// 	if (ray.dir_modifier.x == 1)
-// 		ray.map_intersection.x = ((cubed->player.location.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE);// should this just be -1?? ****changed to + CS
-// 	else
-// 		ray.map_intersection.x = (cubed->player.location.x / CELL_SIZE) * CELL_SIZE - 1;// causes segfault when I change this...
-
-// 	ray.grid_intersection.x = ray.map_intersection.x / CELL_SIZE;
-// 	ray.x_inc = ft_abs(cubed->player.location.x - ray.map_intersection.x);//do I want abs value here?/////* ray.dir_modifier.y
-// 	printf("RAY INC X: %f\n", ray.x_inc);
-// 	// ray.map_intersection.y = ray.x_inc * tan(deg_to_rad(angle));//check about 0 values here // does this work for decreases???
-// 	ray.map_intersection.y = cubed->player.location.y + (ft_abs(ray.x_inc * tan(deg_to_rad(angle))) * ray.dir_modifier.y);//check about 0 values here // does this work for decreases???
-// 	ray.grid_intersection.y = (ray.map_intersection.y / CELL_SIZE);
-	
-// 	//PRINT to CHECK
-// 	printf("PLAYER LOCATION X,Y: %d,%d\n", cubed->player.location.x, cubed->player.location.y);
-// 	printf("FIRST INTERSECTION:\n");
-// 	printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-// 	printf("MODIFIERS X: %i, Y: %i\n", ray.dir_modifier.x, ray.dir_modifier.y);
-// 	cubed->player.location.color = 0xFF00FF;
-// 	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);
-// 	//TODO:save distances
-
-// 	// find second intersection// repeat till grid == wall
-// 	ray.x_inc = CELL_SIZE * ray.dir_modifier.x;
-// 	ray.y_inc = (ft_abs(CELL_SIZE * tan(deg_to_rad(angle)))) * ray.dir_modifier.y;//check for 0 vals///////
-// 	while (ray.map_intersection.y < CELL_SIZE * scene->lines && ray.map_intersection.y > 0) //changed from x
-// 	{	
-// 		ray.map_intersection.y += ray.y_inc;
-// 		ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;
-// 		ray.map_intersection.x += ray.x_inc;
-// 		ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);
-// 		//PRINT to CHECK
-// 		printf("NEXT INTERSECTION:\n");
-// 		printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-// 	}
-// 		cubed->player.location.color = 0xFF00FF;
-// 		ft_bresenham(cubed->player.location, ray.map_intersection, cubed);
-// }
-
-
-
-
-	// //PRINT to CHECK
-	// printf("NEXT INTERSECTION:\n");
-	// printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-	// }
-////seems to be working, copied before cleaning up
-// void	shoot_one_ray_horizontal(t_cubed *cubed, t_scene *scene, float angle)
-// {
-// 	t_ray_calc ray;
-
-// 	// scene = (void *) scene;
-// 	//determine direction
-// 	ray.dir_modifier.x = 1;
-// 	if (angle > 90 && angle < 270)
-// 		ray.dir_modifier.x = -1;
-// 	ray.dir_modifier.y = 1;
-// 	if (angle < 180)//changed to be - for 'up'
-// 		ray.dir_modifier.y = -1;
-// 	//find first intersection
-// 	if (ray.dir_modifier.y == 1)
-// 		ray.map_intersection.y = ((cubed->player.location.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE);// should this just be -1?? ****changed to + CS
-// 	else
-// 		ray.map_intersection.y = (cubed->player.location.y / CELL_SIZE) * CELL_SIZE - CELL_SIZE;
-// 	ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;
-// 	ray.y_inc = (cubed->player.location.y - ray.map_intersection.y);//do I want abs value here?/////* ray.dir_modifier.y
-// 	ray.map_intersection.x = cubed->player.location.x + (ray.y_inc / tan(deg_to_rad(angle)));//check about 0 values here
-// 	ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);
-// 	//PRINT to CHECK
-// 	printf("PLAYER LOCATION X,Y: %d,%d\n", cubed->player.location.x, cubed->player.location.y);
-// 	printf("FIRST INTERSECTION:\n");
-// 	printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-// 	printf("MODIFIERS X: %i, Y: %i\n", ray.dir_modifier.x, ray.dir_modifier.y);
-// 	cubed->player.location.color = 0xFFFFFF;
-// 	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);
-// 	//TODO:save distances
-
-// 	//find second intersection// repeat till grid == wall
-// 	while (ray.map_intersection.y < CELL_SIZE * scene->lines && ray.map_intersection.y > 0)
-// 	{	
-// 	ray.y_inc = CELL_SIZE * ray.dir_modifier.y;
-// 	ray.x_inc = (ft_abs(CELL_SIZE / tan(deg_to_rad(angle)))) * ray.dir_modifier.x;//check for 0 vals
-// 	ray.map_intersection.y += ray.y_inc;
-// 	ray.grid_intersection.y = ray.map_intersection.y / CELL_SIZE;
-// 	ray.map_intersection.x += ray.x_inc;
-// 	ray.grid_intersection.x = (ray.map_intersection.x / CELL_SIZE);
-// 	//PRINT to CHECK
-// 	printf("NEXT INTERSECTION:\n");
-// 	printf("MAP X,Y: %d, %d, GRID X,Y: %i, %i\n", ray.map_intersection.x, ray.map_intersection.y, ray.grid_intersection.x, ray.grid_intersection.y);
-// 	// cubed->player.location.color = 0x999911;
-// 	ft_bresenham(cubed->player.location, ray.map_intersection, cubed);
-// 	}
-// }
