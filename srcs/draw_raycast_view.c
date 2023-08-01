d0399a06ab75da6f234dcadd8fa18ed905ceadc9
Jenny Boucher <jebouche@c2r5p8.hive.fi>
2023-08-01 17:06:44 +0300

    add debug print statment, remove previous attempts to adjust wall height

--- srcs/draw_raycast_view.c
+++ srcs/draw_raycast_view.c
@@ -33,11 +33,12 @@ void	draw_view(t_cubed *cubed, float dist, int x, char side)
 		start.color = 0XFF0000;
 	else
 		start.color = 0XFFF000;
-	line_height = CELL_SIZE * 2 * PROJECTION_HEIGHT / dist; 
+	line_height = CELL_SIZE * PROJECTION_HEIGHT / dist; 
 	if (line_height > PROJECTION_HEIGHT)
 		line_height = PROJECTION_HEIGHT;
 	start.x = x;
-	start.y = cubed->raycast_info->center_of_projection.y + line_height / 2;
+	start.y = cubed->raycast_info->center_of_projection.y - line_height / 2;
+	printf("START XY for wall: %f, %f, center of projection %f\n", start.x, start.y, cubed->raycast_info->center_of_projection.y);
 	//draw line
 	my_put_line_v(cubed->raycast_info->r_img, &start, line_height);
 }
