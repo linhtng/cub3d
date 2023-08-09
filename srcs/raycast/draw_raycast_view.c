627d827810c7a947a382d6bc713e7afc267c499c
Jenny Boucher <jebouche@c3r4p4.hive.fi>
2023-08-09 14:41:41 +0300

    move draw background to raycast setup

--- srcs/raycast/draw_raycast_view.c
+++ srcs/raycast/draw_raycast_view.c
@@ -2,28 +2,10 @@
 #include "ft_raycast.h"
 #include "cub3D.h"
 
-//only need to do once in the begining reuse the background as lonng as we dont raycast anything to it
-void	draw_background(t_cubed *cubed)
-{
-	t_vector start;
-
-	start.x = 0;
-	start.y = 0;
-	start.color = *cubed->scene->ceiling_color;
-	while ((int) start.y < PROJECTION_HEIGHT)
-	{
-		if ((int) start.y > PROJECTION_HEIGHT / 2)
-			start.color = *cubed->scene->floor_color;
-		my_put_line_h(cubed->raycast_info->background_img, &start, PROJECTION_WIDTH);
-		(start.y)++;
-	}
-}
-
 /* 
 	MAP SIZE * PROJECTION WIDTH / DISTANCE OF PLAYER FROM WALL = LINE HEIGHT
 	center line height in projection pane ...
  */
-
 void	draw_view(t_cubed *cubed, float dist, int x, char side)
 {
 	float		line_height;
--- srcs/setup/setup_raycast.c
+++ srcs/setup/setup_raycast.c
@@ -6,13 +6,29 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:18:06 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/09 13:20:13 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/08/09 14:40:48 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_raycast.h"
 #include "cub3D.h"
 
+//only need to do once in the begining reuse the background as lonng as we dont raycast anything to it
+void	draw_background(t_cubed *cubed)
+{
+	t_vector start;
+
+	start.x = 0;
+	start.y = 0;
+	start.color = *cubed->scene->ceiling_color;
+	while ((int) start.y < PROJECTION_HEIGHT)
+	{
+		if ((int) start.y > PROJECTION_HEIGHT / 2)
+			start.color = *cubed->scene->floor_color;
+		my_put_line_h(cubed->raycast_info->background_img, &start, PROJECTION_WIDTH);
+		(start.y)++;
+	}
+}
 void	setup_raycast(t_cubed *cubed, t_raycast *raycast_info)
 {
 	raycast_info->r_img = get_new_image(cubed, PROJECTION_WIDTH, PROJECTION_HEIGHT);
