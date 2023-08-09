3d0dae2b4fc105cac023363bdb4f9f99d45c4622
Jenny Boucher <jebouche@c3r4p4.hive.fi>
2023-08-09 11:35:31 +0300

    remove commented out lines

--- srcs/events/handle_press.c
+++ srcs/events/handle_press.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 18:22:54 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/09 11:30:30 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/08/09 11:35:10 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,11 @@ int	handle_press(int key_code, t_cubed *cubed)
 	if (key_code == FORWARD || key_code == BACK)
 	{
 		move_forward_backward(cubed, key_code);
-		// move_forward(cubed);
 	}
 	if (key_code == LEFT || key_code == RIGHT)
 	{
 		move_right_left(cubed, key_code);
 	}
-	// if (key_code == BACK)
-	// {
-	// 	move_backward(cubed);
-	// }
 	if (cubed->dirty_images)
 		refresh_images(cubed);
 	return (0);
