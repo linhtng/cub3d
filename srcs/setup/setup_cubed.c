e15935dd56f68e0085abb6d3d4d9b11d3b375885
Jenny Boucher <jebouche@c3r3p6.hive.fi>
2023-08-21 15:07:17 +0300

    add zeroing of initial structs and protections for clsoing prog

--- srcs/setup/setup_cubed.c
+++ srcs/setup/setup_cubed.c
@@ -3,17 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   setup_cubed.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thuynguy <thuynguy@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:34:33 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/16 16:45:47 by thuynguy         ###   ########.fr       */
+/*   Updated: 2023/08/21 15:04:38 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+static void	zero_cubed(t_cubed *cubed)
+{
+	cubed->mlx = NULL;
+	cubed->window = NULL;
+	cubed->dirty_images = 0;
+	cubed->raycast_info = NULL;
+	cubed->scene = NULL;
+}
+
 void	setup_cubed(t_cubed *cubed)
 {
+	zero_cubed(cubed);
 	cubed->mlx = mlx_init();
 	cubed->window = mlx_new_window(cubed->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3d");
 }
