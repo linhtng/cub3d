d284be410a0ee472cb80fd73e403bbb73c102113
Jenny Boucher <jebouche@c3r3p6.hive.fi>
2023-08-21 11:29:53 +0300

    norm file

--- srcs/raycast/calculate_rays.c
+++ srcs/raycast/calculate_rays.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:49:54 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/21 10:51:45 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/08/21 11:28:06 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ void	shoot_one_ray_horizontal(t_cubed *cubed, t_ray_calc *ray)
 	ray->cotan = 1.0f / tan(deg_to_rad(ray->angle));
 	if (sin(deg_to_rad(ray->angle)) > 0.001f)
 	{
-		ray->h_map.y = ((float)((int)cubed->scene->player.location.y / CELL_SIZE) * (float)CELL_SIZE) - 0.0001f;
+		ray->h_map.y = (int)cubed->scene->player.location.y / CELL_SIZE;
+		ray->h_map.y = (ray->h_map.y * (float)CELL_SIZE) - 0.0001f;
 		ray->h_map.x = cubed->scene->player.location.x + \
 		((cubed->scene->player.location.y - ray->h_map.y) * ray->cotan);
 		ray->hd.y = (float)-CELL_SIZE;
@@ -50,8 +51,10 @@ void	shoot_one_ray_horizontal(t_cubed *cubed, t_ray_calc *ray)
 	}
 	else if (sin(deg_to_rad(ray->angle)) < -0.001)
 	{
-		ray->h_map.y = (float)((int)cubed->scene->player.location.y / CELL_SIZE) * (float)CELL_SIZE + (float)CELL_SIZE;
-		ray->h_map.x = cubed->scene->player.location.x + ((cubed->scene->player.location.y - ray->h_map.y) * ray->cotan);
+		ray->h_map.y = (int)cubed->scene->player.location.y / CELL_SIZE;
+		ray->h_map.y = (ray->h_map.y * (float)CELL_SIZE) + (float)CELL_SIZE;
+		ray->h_map.x = cubed->scene->player.location.x + \
+		((cubed->scene->player.location.y - ray->h_map.y) * ray->cotan);
 		ray->hd.y = (float)CELL_SIZE;
 		ray->hd.x = -ray->hd.y * ray->cotan;
 	}
@@ -59,7 +62,6 @@ void	shoot_one_ray_horizontal(t_cubed *cubed, t_ray_calc *ray)
 	{
 		ray->h_map.x = cubed->scene->player.location.x;
 		ray->h_map.y = cubed->scene->player.location.y;
-		return ;
 	}
 	check_hit_wall(cubed, &ray->h_grid, &ray->h_map, &ray->hd);
 }
@@ -89,43 +91,46 @@ void	shoot_one_ray_vertical(t_cubed *cubed, t_ray_calc *ray)
 	{
 		ray->v_map.x = cubed->scene->player.location.x;
 		ray->v_map.y = cubed->scene->player.location.y;
-		return ;
 	}
 	check_hit_wall(cubed, &ray->v_grid, &ray->v_map, &ray->vd);
 }
+
 void	get_corrected_shortest(t_cubed *cubed, t_ray_calc *ray_info)
 {
 	float	h_distance;
 	float	v_distance;
-	
+
 	h_distance = get_distance(&cubed->scene->player.location, &ray_info->h_map);
 	v_distance = get_distance(&cubed->scene->player.location, &ray_info->v_map);
 	if (h_distance != 0.0f && (h_distance < v_distance || v_distance == 0.0f))
 	{
 		ray_info->shortest = 'h';
-		ray_info->distance = h_distance * cos(deg_to_rad(ray_info->angle - cubed->scene->player.angle));
+		ray_info->distance = h_distance * \
+		cos(deg_to_rad(ray_info->angle - cubed->scene->player.angle));
 	}
 	else
 	{
 		ray_info->shortest = 'v';
-		ray_info->distance = v_distance * cos(deg_to_rad(ray_info->angle - cubed->scene->player.angle));
+		ray_info->distance = v_distance * \
+		cos(deg_to_rad(ray_info->angle - cubed->scene->player.angle));
 	}
 }
 
 void	cast_rays(t_cubed *cubed)
 {
-	t_ray_calc ray;
-	int		rays_drawn;
+	t_ray_calc	ray;
+	int			rays_drawn;
 
 	ray.angle = correct_degrees(cubed->scene->player.angle - FOV / 2);
 	rays_drawn = 0;
-	while ( rays_drawn <= PROJECTION_WIDTH)
+	while (rays_drawn <= PROJECTION_WIDTH)
 	{
 		shoot_one_ray_horizontal(cubed, &ray);
 		shoot_one_ray_vertical(cubed, &ray);
 		get_corrected_shortest(cubed, &ray);
 		draw_view(cubed, &ray, PROJECTION_WIDTH - rays_drawn);
-		ray.angle = correct_degrees(ray.angle + cubed->raycast_info->angle_between_rays);
+		ray.angle = \
+		correct_degrees(ray.angle + cubed->raycast_info->angle_between_rays);
 		rays_drawn++;
 	}
 }
