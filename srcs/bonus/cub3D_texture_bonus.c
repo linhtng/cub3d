4fdab1fbd66187e4be94451d6fcebd6c6fd05982
Jenny Boucher <jebouche@c3r4p4.hive.fi>
2023-08-22 11:07:28 +0300

    floor and ceiling casting working, issue on walls

--- srcs/bonus/cub3D_texture_bonus.c
+++ srcs/bonus/cub3D_texture_bonus.c
@@ -6,11 +6,11 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 20:10:04 by thuynguy          #+#    #+#             */
-/*   Updated: 2023/08/21 14:08:16 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/08/22 10:59:35 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D.h"
+#include "cub3D_bonus.h"
 
 void	ft_pixel_put(t_img_data *data, int x, int y, unsigned int color)
 {
@@ -28,7 +28,10 @@ unsigned int	ft_pixel_get(t_img_data *data, int x, int y)
 	unsigned int	dst;
 
 	if (x >= data->width || x < 0 || y >= data->height || y < 0)
-		return (0XFF0000);
+	{
+		// printf("[FT PIXEL GET] Out of bounds: x %ii, y %i \n", x, y);//
+		return (RED);
+	}
 	dst = *(unsigned int *)(data->addr + (y * data->line_length + x * \
 	(data->bits_per_pixel / 8)));
 	return (dst);
@@ -58,52 +61,33 @@ float line_size)
 		texture.x = TEXTURE_SIZE - 1 - (((int) hit->y) % TEXTURE_SIZE);
 	else
 		texture.x = ((int) hit->y) % TEXTURE_SIZE;
+	if (texture.x >= 64 || texture.x < 0 || texture.y >= 64 || texture.y < 0)//
+		printf("[GET TEXTURE VEC] tex.x %f, tex.y %f \n", texture.x, texture.y);//
 	return (texture);
 }
 
-void draw_floors(t_cubed *cubed, t_ray_calc *ray, float line_height)
+void	b_draw_textured_walls(t_cubed *cubed, t_ray_calc *ray, t_draw_info *d_info)
 {
-	t_vector	tex;
-	float		delta_y;
-	float		ray_fix;
-	
-	if (line_height == PROJECTION_HEIGHT)
-		return ;
-	tex.y = cubed->raycast_info->center_of_projection.y - line_height / 2 + line_height;
-	delta_y = tex.y - cubed->raycast_info->center_of_projection.y - line_height / 2 - PROJECTION_HEIGHT / 2.0f;
-	ray_fix = cos(deg_to_rad(correct_degrees(cubed->scene->player.angle - ray->angle)));
-	while (tex.y < PROJECTION_HEIGHT)
-	{
-		tex.x = cubed->scene->player.location.x / 2 + cos(deg_to_rad(ray->angle)) * (PROJECTION_HEIGHT / 2 - 2) / delta_y / ray_fix;
-	}
-}
-
-void	draw_textured_walls(t_cubed *cubed, int project_x, t_ray_calc *ray, \
-int dir)
-{
-	float		line_height;
 	t_vector	pane;
 	float		y_step;
-	t_vector	texture;
 	int			index;
 
-	line_height = CELL_SIZE * PROJECTION_HEIGHT / ray->distance;
-	y_step = TEXTURE_SIZE / line_height;
+	y_step = TEXTURE_SIZE / d_info->line_height;
 	if (ray->shortest == 'v')
-		texture = get_texture_vec(&ray->v_map, dir, y_step, line_height);
+		d_info->texture = get_texture_vec(&ray->v_map, d_info->dir, y_step, d_info->line_height);
 	else
-		texture = get_texture_vec(&ray->h_map, dir, y_step, line_height);
-	if (line_height > PROJECTION_HEIGHT)
-		line_height = PROJECTION_HEIGHT;
-	pane.y = cubed->raycast_info->center_of_projection.y - line_height / 2;
+		d_info->texture = get_texture_vec(&ray->h_map, d_info->dir, y_step, d_info->line_height);
+	if (d_info->line_height > PROJECTION_HEIGHT)
+		d_info->line_height = PROJECTION_HEIGHT;
+	pane.y = cubed->raycast_info->center_of_projection.y - d_info->line_height / 2;
 	index = 0;
-	while (index < line_height)
+	while (index < d_info->line_height)
 	{
-		pane.color = ft_pixel_get(cubed->scene->texture[dir], \
-		texture.x, texture.y);
-		ft_pixel_put(cubed->raycast_info->r_img, project_x, pane.y, pane.color);
+		pane.color = ft_pixel_get(cubed->scene->texture[d_info->dir], \
+		d_info->texture.x, d_info->texture.y);
+		ft_pixel_put(cubed->raycast_info->r_img, d_info->project_x, pane.y, pane.color);
 		index++;
 		pane.y++;
-		texture.y += y_step;
+		d_info->texture.y += y_step;
 	}
 }
