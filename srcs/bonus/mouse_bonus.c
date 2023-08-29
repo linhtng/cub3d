b340c44fe09458b4c2ad998d1368b33e81ec80f8
Jenny Boucher <jebouche@c2r2p8.hive.fi>
2023-08-29 16:42:15 +0300

    add fix for player direction and mouse movement

--- srcs/bonus/mouse_bonus.c
+++ srcs/bonus/mouse_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thuynguy <thuynguy@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 19:10:01 by thuynguy          #+#    #+#             */
-/*   Updated: 2023/08/29 15:38:50 by thuynguy         ###   ########.fr       */
+/*   Updated: 2023/08/29 16:41:47 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ int	mouse_move(int x, int y, void *param)
 
 	cubed = (t_cubed_bonus *)param;
 	if (y >= 0 && y < PROJECTION_HEIGHT && x >= 0 && x < PROJECTION_WIDTH)
+	{
 		cubed->scene->player.angle = correct_degrees(cubed->scene->player.angle \
 		- (x - cubed->mouse_x) / 3);
+		cubed->scene->player.d.x = \
+		cos(deg_to_rad(cubed->scene->player.angle)) * 5;
+		cubed->scene->player.d.y = \
+		-sin(deg_to_rad(cubed->scene->player.angle)) * 5;
+	}
 	cubed->mouse_x = x;
 	return (0);
 }
