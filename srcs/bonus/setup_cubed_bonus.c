54ef8d9ad9ccbbbf951a1015ad9ca3838b8c0f42
Jenny Boucher <jebouche@c3r2p6.hive.fi>
2023-08-30 10:36:48 +0300

    add protection for mlx and window failure

--- srcs/bonus/setup_cubed_bonus.c
+++ srcs/bonus/setup_cubed_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   setup_cubed_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thuynguy <thuynguy@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:34:33 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/26 19:13:49 by thuynguy         ###   ########.fr       */
+/*   Updated: 2023/08/30 10:35:03 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,11 @@ void	setup_cubed(t_cubed *cubed)
 
 	zero_cubed((t_cubed_bonus *) cubed);
 	cubed->mlx = mlx_init();
+	if (!cubed->mlx)
+		mlx_close(cubed, 1, "intialization of mlx failed");
 	cubed->window = mlx_new_window(cubed->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3d");
+	if (!cubed->window)
+		mlx_close(cubed, 1, "intialization of mlx window failed");
 	((t_cubed_bonus *)cubed)->minimap_img = \
 	get_new_image(cubed, MINI_MAP_DIAMETER, MINI_MAP_DIAMETER);
 	((t_cubed_bonus *)cubed)->mini_player_img = \
--- srcs/setup/setup_cubed.c
+++ srcs/setup/setup_cubed.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 12:34:33 by jebouche          #+#    #+#             */
-/*   Updated: 2023/08/30 10:15:09 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/08/30 10:35:10 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ void	setup_cubed(t_cubed *cubed)
 {
 	zero_cubed(cubed);
 	cubed->mlx = mlx_init();
+	if (!cubed->mlx)
+		mlx_close(cubed, 1, "intialization of mlx failed");
 	cubed->window = mlx_new_window(cubed->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3D");
+	if (!cubed->window)
+		mlx_close(cubed, 1, "intialization of mlx window failed");
 }
